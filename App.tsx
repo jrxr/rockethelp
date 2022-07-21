ff38458c423d459bd6eafd35123c9c6a21df650f
Agacy junior <juniormartinssom@gmail.com>
2022-07-20 21:34:36 -0300

    Finish aula 2

--- App.tsx
+++ App.tsx
@@ -7,8 +7,10 @@ import {
 
 import { THEME } from "./src/styles/theme";
 
-import { Home } from "./src/screens/Home";
 import { Loading } from "./src/components/Loading";
+import React from "react";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,12 +20,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
